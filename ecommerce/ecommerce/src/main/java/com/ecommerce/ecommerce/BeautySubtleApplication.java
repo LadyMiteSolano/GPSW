3de8a4b885c9988ffb9baa0984ef4b50d52f70d1
LadyMiteSolano <ladymitesolano@gmail.com>
2024-01-22 13:51:53 -0500

    Creación de proyecto

---
+++ ecommerce/ecommerce/src/main/java/com/ecommerce/ecommerce/BeautySubtleApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySubtleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySubtleApplication.class, args);
+	}
+
+}
---
+++ ecommerce/ecommerce/src/test/java/com/ecommerce/ecommerce/BeautySubtleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySubtleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
